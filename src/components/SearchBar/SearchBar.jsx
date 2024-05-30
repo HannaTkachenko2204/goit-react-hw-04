3f071646ee42ebd8336dc61acf1cecefb397fdb3
Анна <anna.kuvakina91@gmail.com>
2024-05-30 21:36:03 +0300

    task-1

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,18 @@
+const SearchBar = ({onSubmit}) => {
+    console.log(onSubmit);
+  return (
+    <header>
+      <form>
+        <input
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </header>
+  )
+}
+
+export default SearchBar
